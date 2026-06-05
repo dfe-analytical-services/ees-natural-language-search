8d4466d1904583b36eed6ce047545fe1d6c01e7c
Ben Outram <ben@hiveit.co.uk>
2026-06-05 20:18:34 +0100

    EES-7164 Add Deploy stage to Azure Pipeline (#6)
    * EES-7164 Parameterise artifactName which needs to be common across CI and Deploy stages
    
    * EES-7164 Add Deploy stage to Azure Pipeline

--- routes/healthcheck.py
+++ routes/healthcheck.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-@router.get("/health_check")
+@router.get("/api/HealthCheck")
 async def health_check():
     return {"message": "API working"}
 
